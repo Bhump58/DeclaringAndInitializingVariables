507ee627e2b8424898adf898e1e273c42ea91cf7
Brian H <humphreybrian58@gmail.com>
2020-08-21 18:58:22 -0400

    Final Addition to the project

--- DeclareAndInitializeVariables/Program.cs
+++ DeclareAndInitializeVariables/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DeclareAndInitializeVariables
 {
     class Program
     {
+        public static void DrinkingAge(int age)
+        {
+            if (age >= 21)
+            {
+                Console.WriteLine($"Ohh, you're {age} years old let's turn up!!");
+            }
+            else
+            {
+                Console.WriteLine("Please come back when you are old enough.");
+            }
+        }
         static void Main(string[] args)
         {
             string name = "Brian";
@@ -16,8 +28,15 @@ namespace DeclareAndInitializeVariables
 
             Console.WriteLine($"Hello my name is {name}, and I am {age} years old");
             Console.WriteLine($"Fun fact about me is my favorite letter is {favoriteLetter}, " +
-                $"and if you ask my friends if I am fun they will say it is {isFun}");
+                $"and if you ask my friends if I am fun they will say it is {isFun}.");
             Console.WriteLine($"My height in feet is {heightInFeet} Ft, but in inches that is  {heightInInches}.");
+
+            DrinkingAge(12);
+
+
+
+
+            
         }
     }
 }
